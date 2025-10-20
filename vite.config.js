a9c637ac9053ddf31ae53a30a42643b3ec876fe1
Vintage Rolodex <user@example.com>
2025-10-20 20:30:23 +0200

    Roo9

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/vintage-rolodex-demo/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
@@ -15,5 +15,18 @@ export default defineConfig({
   build: {
     assetsDir: 'assets',
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
   }
 })
